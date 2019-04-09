cc0bc88b9cf2004edbb257142a7b63789951cbaf
xiongxiaowen <xiongxiaowen@didichuxing.com>
2019-04-09 16:24:06 +0800

    test(spec): update test logic

---
+++ src/core/schedulers/AsyncScheduler.js
@@ -0,0 +1,74 @@
+import { Subscription } from '@/core'
+
+export class AsyncScheduler {
+  constructor(AsyncAction) {
+    this.AsyncAction = AsyncAction
+    this.actionQueue = []
+    this.isExecutingWork = false
+  }
+  flushQueue(action) {
+    if (this.isExecutingWork) {
+      this.actionQueue.push(action)
+      return
+    }
+    this.isExecutingWork = true
+    do {
+      action.execute(action.state)
+    } while (action = this.actionQueue.shift())
+    this.isExecutingWork = false
+  }
+  schedule(work, delay, state) {
+    const asyncAction = new this.AsyncAction(this, work)
+    return asyncAction.schedule(state, delay)
+  }
+}
+export class AsyncAction extends Subscription {
+  constructor(scheduler, work) {
+    super()
+    this.scheduler = scheduler
+    this.work = work
+
+    this.delay = undefined
+    this.state = undefined
+    this.timerId = undefined
+  }
+  schedule(state, delay) {
+    /* 在delay后执行this.work， 传入的值为this.state */
+    if (this.hasUnsubscribed) return
+    this.state = state
+    this.delay = delay
+
+    this.timerId = this.setupAsyncTask(this.scheduler, delay)
+    return this
+  }
+  setupAsyncTask(scheduler, delay = 0) {
+    /* 在delay时间后，执行scheduler的flushQueue操作 */
+    return setTimeout(scheduler.flushQueue.bind(scheduler, this), delay)
+  }
+  cancelAsyncTask(id) {
+    /* 取消任务 */
+    clearTimeout(id)
+  }
+  execute(state) {
+    /* 执行work */
+    this.work(state)
+  }
+  _unsubscribe() {
+    /* 取消任务 */
+    const timerId = this.timerId
+    const scheduler = this.scheduler
+    const index = scheduler.actions.indexOf(this)
+
+    this.task = null
+    this.scheduler = null
+    this.state = undefined
+    if(index !== -1) {
+      scheduler.actions.splice(index, 1)
+    }
+    if(timerId != null) {
+      this.cancelAsyncTask(timerId)
+    }
+    this.delay = null
+   }
+}
+export const async = new AsyncScheduler(AsyncAction)
--- test/core/basic.spec.js
+++ test/core/basic.spec.js
@@ -7,19 +7,17 @@ it('should pass basic test', done => {
   const subscriber = {
     next(value) {
       expect(value).toEqual(1)
-      done()
     },
     error(reason) {
       console.error(reason)
     },
-    complete() {
-      console.log('complete!')
-    }
+    complete: done
   }
 
   const ob$ = new Observable(subscriber => {
     const timer = setTimeout(() => {
       subscriber.next(1)
+      subscriber.complete()
     }, 1000)
 
     const unsubscribe = () => {
@@ -38,15 +36,12 @@ it('should never be called when unsubscribed', (done) => {
     error(reason) {
       console.error(reason)
     },
-    complete() {
-      console.log('complete!')
-    }
+    complete: () => {}
   }
-
   const ob$ = new Observable(subscriber => {
     const timer = setTimeout(() => {
-      subscriber.next(i)
-    }, 3000)
+      subscriber.next(1)
+    }, 300)
     const unsubscribe = () => {
       clearTimeout(timer)
     }
@@ -55,10 +50,10 @@ it('should never be called when unsubscribed', (done) => {
   const sub = ob$.subscribe(subscriber)
   setTimeout(() => {
     sub.unsubscribe()
-  }, 1000)
+  }, 100)
   setTimeout(() => {
     expect(mockNext).not.toBeCalled()
     done()
-  }, 4000)
+  }, 400)
 })
 
